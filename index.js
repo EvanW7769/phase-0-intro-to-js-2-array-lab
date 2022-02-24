8e9df1ac3ca05e5cb9166e7b7198c9584ba647ae
Evan Watson <thomasevanwatson@gmail.com>
2022-02-24 12:51:14 -0500

    commit tree d21eb876ce5648b1cdf9e9b88e910dc43c3aba59

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+cats.push(name)
+return cats
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+    let appendedCats = [...cats, name]
+    return appendedCats
+}
+
+function prependCat(name){
+    let prependedCats = [name, ...cats]
+return prependedCats
+}
+
+
+function removeLastCat(){
+    let removedCat = cats.slice(0,2)
+    return removedCat
+}
+function removeFirstCat(){
+let removedFirstCat = cats.slice(1,3) 
+return removedFirstCat
+}
+
